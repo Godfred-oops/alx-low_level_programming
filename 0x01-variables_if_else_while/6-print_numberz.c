dc2fdbd8c9817e27209354344e446da4b15e2c48
Godfred-oops <ababiogodfred332@gmail.com>
2022-07-02 02:36:41 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int s;
+
+	for (s = 0; s < 10; s++)
+		putchar(s);
+	putchar("\n");
+	return (0);
+}
