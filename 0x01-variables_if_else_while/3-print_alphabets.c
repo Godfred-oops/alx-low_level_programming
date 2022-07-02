439ccd0ea644a0823b3bf9f20bd4fe690d37f2b8
Godfred-oops <ababiogodfred332@gmail.com>
2022-07-02 05:47:49 -0700

    three-alpahbet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -17,6 +17,5 @@ int main(void)
 		putchar(low);
 
 	putchar('\n');
-
 	return (0);
 }
