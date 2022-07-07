9ebe7a3ec3ce22e708d9a858e3d4b839527d7bdd
Godfred-oops <ababiogodfred332@gmail.com>
2022-07-07 15:51:59 -0700

    completd task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiples two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a * b;
+
+	return (prod);
+}
+
