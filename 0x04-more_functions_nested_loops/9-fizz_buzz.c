06a284fd5f2c9446b9fbc3c552320bc4b78273b2
Godfred-oops <ababiogodfred332@gmail.com>
2022-07-08 16:07:53 -0700

    completed task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,18 +8,24 @@
 
 int main(void)
 {
-	if (i % 3 == 0 && i % 5 == 0)
+
+	int i;
+
+	for (i = 1; i < 100; i++)
 	{
-		printf("FizzBuzz ");
 
-	else if (i % 3 == 0)
-		printf("Fizz ");
+		if (i % 3 == 0 && i % 5 == 0)
 
-	else if (i % 5 == 0)
-		printf("Buzz ");
+			printf("FizzBuzz ");
 
-	else
-		printf("%d ", i);
+		else if (i % 3 == 0)
+			printf("Fizz ");
+
+		else if (i % 5 == 0)
+			printf("Buzz ");
+
+		else
+			printf("%d ", i);
 	}
 	printf("Buzz\n");
 
