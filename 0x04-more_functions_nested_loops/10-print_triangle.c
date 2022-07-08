7be4a83153b0541da6aaef2f0c49bac6ab479897
Godfred-oops <ababiogodfred332@gmail.com>
2022-07-08 16:38:58 -0700

    completed task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,23 +9,21 @@
 
 void print_triangle(int size)
 {
-	int j, n = size - 1;
+	int j, n;
 
 	if (size > 0)
 	{
-		for (i = 0; i < size; i++)
+		for (j = 1; j <= size; j++)
 		{
-			for (j = 0; j < size; j++)
-			{
-				if (j < n)
-					_putchar(' ');
-				else
-					_putchar('#');
-			}
-			n--;
+			for (n = size - j; n > 0; n--)
+				_putchar(' ');
+
+			for (n = 0; n < j; n++)
+				_putchar('#');
+
+			if (j == size)
+				continue;
 			_putchar('\n');
 		}
 	}
-	else
-		_putchar('\n');
 }
