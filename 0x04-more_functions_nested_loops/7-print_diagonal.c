8fe84908f722d1ac90071a3507c21a83ec4dd1d5
Godfred-oops <ababiogodfred332@gmail.com>
2022-07-08 07:57:03 -0700

    completed task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: character to be checked
+ *
+ * Return: Alays 0
+ */
+
+void print_diagonal(int n)
+{
+	int len, s;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+		{
+			for (s = 0; s < len; s++)
+				_putchar(' ');
+			_putchar('\\');
+
+			if (len == n - 1)
+				continue;
+			_putchar('\n');
+		}
+	}
+}
