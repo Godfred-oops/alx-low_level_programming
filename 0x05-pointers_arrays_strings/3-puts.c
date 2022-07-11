8969e3cadd3d462968a233a96a80b3dce2199cc6
Godfred-oops <ababiogodfred332@gmail.com>
2022-07-11 16:26:40 -0700

    completed task 03

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by new line
+ *@str: string to be checked
+ *
+ * Return: Nothing
+ */
+
+void _puts(char *str)
+{
+	int i = = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
+
+
