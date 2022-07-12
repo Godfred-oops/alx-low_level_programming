f4b8ee8a09a2d6f9c8b652d5dc0003c59e069f12
Godfred-oops <ababiogodfred332@gmail.com>
2022-07-12 15:44:41 -0700

    completed task 05

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - reverses a string
+ * @s: character to be checked
+ * Return: Nothing
+ */
+
+void rev_string(char *s)
+{
+	int len = 0, i = 0;
+	char a;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len--)
+	{
+		a = s[i];
+		s[i++] = s[len];
+		s[len] = a;
+	}
+}
