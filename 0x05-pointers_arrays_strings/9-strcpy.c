8dfebde76fd5ea7488f149e7ac5625ee85f9cf1d
Godfred-oops <ababiogodfred332@gmail.com>
2022-07-12 17:35:34 -0700

    completed task 09

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,10 +8,10 @@
  * @dest: A pointer to destination of string
  * @src: A pointer to source string to copy from
  *
- * Retrun: pointer to dest
+ * Return: pointer to dest
  */
 
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest,  const char *src)
 {
 	int index = 0;
 
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -14,6 +14,6 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
+char *_strcpy(char *dest, const char *src);
 
 #endif
