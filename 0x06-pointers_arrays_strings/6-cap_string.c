6a3d8b67503cb6ab862ae918630d1859b8d82c04
Godfred-oops <ababiogodfred332@gmail.com>
2022-07-14 11:00:59 -0700

    completed task 06

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-int check_seperators(char c);
 
 /**
  * cap_string - a function that capitalizes all words of a string
@@ -13,37 +12,27 @@ char *cap_string(char *s)
 
 	while (s[i])
 	{
-		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
+		while (!(s[i] >= 'a' && s[i] <= 'z'))
+			i++;
+
+		if (s[i - 1] == ' ' ||
+		    s[i - 1] == '\t' ||
+		    s[i - 1]  == '\n' ||
+		    s[i - 1] == ',' ||
+		    s[i - 1] == ';' ||
+		    s[i - 1] == '.' ||
+		    s[i - 1] == '!' ||
+		    s[i - 1] == '?' ||
+		    s[i - 1] == '"' ||
+		    s[i - 1] == '(' ||
+		    s[i - 1] == ')' ||
+		    s[i - 1] == '{' ||
+		    s[i - 1]  == '}' ||
+		    i == 0)
 			s[i] -= 32;
 
-		if (check_seperators(s[i] && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
-				s[i + 1] -= 32;
 		i++;
 	}
-
 	return (s);
 }
-
-/**
- * check_seperators - separators of words: space, tabulation, new line
- * ,, ;, ., !, ?, ", (, ), {, and }
- * @c: an input character
- * Return: 1 if seperator, 0 otherwise
- */
-
-int check_seperators(char c)
-{
-	int i = 0;
-	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
-
-			'"', '(', ')', '{', '}' };
-
-	for (; i < 13, i++)
-	{
-		if (c == seperators[i])
-			return (1);
-	}
-	return (0);
-}
-
 
