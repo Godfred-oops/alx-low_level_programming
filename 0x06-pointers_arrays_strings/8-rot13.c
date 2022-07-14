c6a77b87fd97bc45558e36e56e79631c540d5125
Godfred-oops <ababiogodfred332@gmail.com>
2022-07-14 12:28:09 -0700

    completed task 08

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * rot13 - a function that encodes a string using rot13
+ * @s: an input string to encode using rot13
+ * Return: an encode string
+ */
+
+char *rot13(char *s)
+{
+	int i = 0;
+
+	while ([i] != '\0')
