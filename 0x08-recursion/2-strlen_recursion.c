151716eaf162f713920b7c3c23d48c9170c88b8a
Godfred-oops <ababiogodfred332@gmail.com>
2022-07-20 12:01:15 -0700

    completed task 02

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns length of a string
+ * @s: string to be checked
+ *
+ * Return: Nothing
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+
+	return (len);
+}
