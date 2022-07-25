b4cab985b25485c84109d99530651478bbeb5b8a
Godfred-oops <ababiogodfred332@gmail.com>
2022-07-25 16:36:45 -0700

    completed task 00

---
+++ 0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name. followed by new line
+ * @argc: the number of characters supplied
+ * @argv: array of pinters to the argument
+ *
+ * Return: Always 0
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
