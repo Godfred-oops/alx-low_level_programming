341c310928e042e8a3ec1c2138a6049f55cb0ab5
Godfred-oops <ababiogodfred332@gmail.com>
2022-07-25 17:42:30 -0700

    completed task 02

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,8 +13,10 @@ int main(int argc, char *argv[])
 	int num1, num2, prod;
 
 	if (argc != 3)
+	{
 		printf("Error\n");
-	return (1);
+		return (1);
+	}
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
