12b0a5c53f9890401525d0f512dd2b712aabd73c
Godfred-oops <ababiogodfred332@gmail.com>
2022-07-25 17:04:20 -0700

    completed task 01

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <std.io.h>
+
+/**
+ *  main - prints the number of arguments passed
+ *  @argc: the number of arguments
+ *  @argv: the array of string
+ *
+ *  Return: always 0
+ */
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
+
